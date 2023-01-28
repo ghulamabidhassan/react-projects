e0029ca62e2611f0f4f2b05c2fea3be68be4309a
Abid <ghulamabidhassan@gmail.com>
2023-01-28 19:49:55 +0400

    small tweaks

--- Color Generator/src/SingleColor.js
+++ Color Generator/src/SingleColor.js
@@ -2,15 +2,33 @@ import React, { useState, useEffect } from "react";
 // import rgbToHex from "./utils";
 
 const SingleColor = ({ weight, hex, index }) => {
+  let hexColor = `#${hex}`;
+  const [alert, setAlert] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [alert]);
+
   return (
     <article
       style={{
         backgroundColor: `#${hex}`,
       }}
       className={`article ${index > 10 && "light"}`}
+      onClick={() => {
+        navigator.clipboard.writeText(hexColor);
+        setAlert(true);
+      }}
     >
       <h4 className="weight">{weight}%</h4>
-      <p className="hex-code">#{hex}</p>
+      <p className="hex-code">{hexColor}</p>
+      {alert && <p className="copy">copied to clipboard</p>}
     </article>
   );
 };
